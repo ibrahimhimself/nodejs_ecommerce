8565a6b8021b30f28acb1e4ac5c14f1ff0a751d8
Ahmed Mohamed Ibrahim <amoibrahim22@gmail.com>
2022-07-24 16:54:24 +0200

    Search Function

--- Controllers/Home.controller.js
+++ Controllers/Home.controller.js
@@ -12,21 +12,21 @@ exports.getHome = (req,res,next) => {
     })
 
     // let searchBySellerName = req.query.Seller;
-    // let searchByName = req.query.name;
-    // console.log(searchByName);
-    // if(searchByName && searchByName!== ""){
-    //     var sql = `Select * from product where Seller = ${searchBySellerName} || where name = ${searchByName}`
-    //     db.query(sql,(err,result)=>{
-    //     if(err) throw err
-    //     res.render('index',{product:result})
-    // })
-    // }
-    // else
-    // {
-    //     var sql = "Select * from product"
-    //     db.query(sql,(err,result)=>{
-    //     if(err) throw err
-    //     res.render('index',{product:result})
-    //     })
-    // }
+    let searchByName = req.query.name;
+    console.log(searchByName);
+    if(searchByName && searchByName!== ""){
+        var sql = `Select * from product where Seller like  %${searchBySellerName}% or  name like %${searchByName}%`
+        db.query(sql,(err,result)=>{
+        if(err) throw err
+        res.render('index',{product:result})
+    })
+    }
+    else
+    {
+        var sql = "Select * from product"
+        db.query(sql,(err,result)=>{
+        if(err) throw err
+        res.render('index',{product:result})
+        })
+    }
 }
